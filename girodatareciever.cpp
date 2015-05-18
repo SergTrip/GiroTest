873219e1328cefa593442c1e97eace2b12c5d7ae
serg_trip <serg_trip@mail.ru>
2015-05-18 22:26:58 +0200

    Настроен прием данных

--- girodatareciever.cpp
+++ girodatareciever.cpp
@@ -1,12 +1,121 @@
 #include "girodatareciever.h"
 
-GiroDataReciever::GiroDataReciever()
+GiroDataReciever::GiroDataReciever() :
+    m_eRecieverState    ( Disconnected  ),
+    m_bDataDtreamSync   ( false         )
 {
+    // Сбросить  структур
+    // m_structLastPackedge;
 
+    // Подключить таймер к слоту
+    connect( &m_oStateTimer, SIGNAL(timeout()), this, SLOT(stateTimeOutSlot()));
+
+    // Подключить сигнал приема данных
+    connect(  this,   SIGNAL    (   readyRead()         ),
+              this,   SLOT      (   recieveDataSlot()   )   );
+
+    // Запускаем таймер
+    m_oStateTimer.start( GIRO_DATA_STATE_TIME );
 }
 
 GiroDataReciever::~GiroDataReciever()
 {
+    // Отключиться
+    this->close();
+}
 
+void GiroDataReciever::stateTimeOutSlot()
+{
+    // Проверить состояние
+    switch( m_eRecieverState )
+    {
+    // Если устройство отсоеденино
+    case Disconnected:
+        // Обновить список портов
+        m_oPortList.clear();
+        m_oPortList = QSerialPortInfo::availablePorts();
+
+        // Поемнять состояние
+        m_eRecieverState = CheckPorts;
+        break;
+
+    // Если идет проверка портов
+    case CheckPorts:
+        do
+        {
+            // Если список пустой
+            if( m_oPortList.isEmpty() )
+            {
+                // Поемнять состояние
+                m_eRecieverState = Disconnected;
+                // Прервать цикл
+                break;
+            }
+
+            // Подключить следующий порт
+            this->setPortName( m_oPortList.first().portName() );
+            // Вывести имя
+            qDebug() << "Подключем порт: " << m_oPortList.first().portName() ;
+
+            // Удалить его из списка
+            m_oPortList.removeFirst();
+
+            // Сбросить счетчик пакетов
+            m_nPackageCounter = 0;
+
+        }
+        while( !open(QIODevice::ReadWrite) );
+        break;
+
+    // Если подсоединились
+    case Connected:
+        // Проверить соединение ???
+        qDebug() << " Синхронизировано";
+        break;
+
+    default:
+        // Что то не так - сообщить
+        qDebug() << " GiroDataReciever::stateTimeOutSlot() Default Case ";
+        break;
+    }
+}
+
+void GiroDataReciever::recieveDataSlot()
+{
+    // Вычитать буфер
+    QByteArray tempBuffer = readAll();
+    qDebug() << "Сервер: Приняты байты...";
+
+    // Добавляем полученные данные
+    m_oRecieveBuffer.append( tempBuffer );
+
+    // Пока длинна буфера больше 2
+    while( m_oRecieveBuffer.size() >= 8 )
+    {
+        // Формируем полученную структуру
+        m_structLastPackedge = *(dataPackageStruct*)m_oRecieveBuffer.data();
+        qDebug() << "Marker = " << QString::number( m_structLastPackedge.marker, 16 );
+        qDebug() << "X = " << m_structLastPackedge.x << "; Y = " << m_structLastPackedge.y << "; Z = " << m_structLastPackedge.z;
+
+        // Если они не равны маркеру
+        if( (m_structLastPackedge.marker == 0x00AA) && !m_bDataDtreamSync )
+        {
+            // Если уже 5 пакетов без ошибок
+            m_nPackageCounter++;
+            if( m_nPackageCounter > 5 )
+            {
+                // Отметить синхронизауию
+                m_bDataDtreamSync = true;
+                // Сменить состояние
+                m_eRecieverState = Connected;
+            }
+        }
+
+        m_oRecieveBuffer.remove( 0, 8 );
+    }
+
+    // Опубликовать последние данные
+
+    // m_oRecieveBuffer.clear();
 }
 
--- girodatareciever.h
+++ girodatareciever.h
@@ -2,12 +2,68 @@
 #define GIRODATARECIEVER_H
 
 #include <QtSerialPort/QtSerialPort>
+#include <QTimer>
+#include <QDebug>
+
+// Время для таймера контроля состояния
+#define GIRO_DATA_STATE_TIME 500
+// Длинна пакета
+#define GIRO_DATA_PACET_SIZE 8
 
 class GiroDataReciever : public QSerialPort
 {
+    Q_OBJECT
 public:
+    // Конструктор
     GiroDataReciever();
     ~GiroDataReciever();
+
+private:
+    // Состояния системы
+    enum
+    {
+        Disconnected,       // Устройство не подключено
+        CheckPorts,         // Идет опрос доступных портов
+        Connected           // Устройство подключено
+    } m_eRecieverState;
+
+    // Таймер для тактирования протокола
+    QTimer m_oStateTimer;
+
+    // Содержит список портов
+    // QSerialPortInfo m_oConnectionList;
+
+    // Содержит список портов
+    QList<QSerialPortInfo>  m_oPortList;
+
+    // Флаг синхронизации данных
+    bool        m_bDataDtreamSync;
+    // Счетчик данных
+    qint8       m_nPackageCounter;
+
+    // Буфер полученных данных
+    QByteArray  m_oRecieveBuffer;
+
+    // Структура пакета
+    typedef struct
+    {
+        quint16 marker; // маркерпакета
+        qint16  x;
+        qint16  y;
+        qint16  z;
+
+    }dataPackageStruct;
+
+    // Экземпляр структуры
+    dataPackageStruct m_structLastPackedge;
+
+protected slots:
+    // Слот для контроля состояния модуля
+    void stateTimeOutSlot();
+
+    // Слот приема данных
+    void recieveDataSlot();
+
 };
 
 #endif // GIRODATARECIEVER_H
